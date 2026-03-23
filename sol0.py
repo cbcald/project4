73d3864855d58dcc02ffddd66e3f823f0dbafaef
Carson Caldwell <cbcald@umich.edu>
2026-03-23 03:25:33 +0000

    good work

--- sol0.py
+++ sol0.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
+
+#write something that will overflow the buffer with 
+#Hi cbcald! Your grade is A+.
+sys.stdout.buffer.write(b'cbcald' + b'\0')
+sys.stdout.buffer.write(b'A' * 3 + b'A+' + b'\0')
+
+
